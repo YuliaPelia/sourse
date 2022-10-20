d39a5b0cc124eac3ac55c658c4e33e77ee2f6300
Yulia <yuliapelia19@gmail.com>
2022-10-20 12:43:44 +0300

    modules

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,11 +1,6 @@
-// 2. Щоб експортувати весь код який буде знаходитися тут, необхідно зробити певну структуру
-// використовуючи звичайні ф-ції
+
 const modals = () => {
-    // 3. пишемо загальний алгоритм який буде приймати в себе різні аргументи і робити те що нам потрібно
-    // створюєм ф-цію яка буде відповідати за привязку модального вікна до певного триггеру
-    // trigger - селектор нашої кнопки по якій ми будемо клікати
-    // modal - модальне вікно яке ми будемо відкривати
-    // close - селекор який закриває модальне вікно (хрестик)
+
     function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
         const trigger = document.querySelectorAll(triggerSelector),
               modal = document.querySelector(modalSelector),
@@ -14,9 +9,8 @@ const modals = () => {
 
         trigger.forEach(item => {
             item.addEventListener('click', (e) => {
-                // e.target - чи точно існує той елемент на який клікнув користувач
                 if(e.target) {
-                    e.preventDefault(); // відміняєм стандартну поведінку браузера
+                    e.preventDefault(); 
                 }
     
                 windows.forEach(item => {
@@ -24,15 +18,11 @@ const modals = () => {
                 });
 
                 modal.style.display = "block";
-                // робимо так що коли модальне вікно відкрито то ми можемо гортати тільки модальне вікно,
-                // якщо воно велике по висоті якщо ні то сторінка просто заморожується і при виклику модального
-                // вікна скролити сторінку буде не можливо
+
                 document.body.style.overflow = "hidden";
-                // document.body.classList.add('modal-open');
     
             });
         });
-        // закриття модального вікна при натисканні на хрестик
         close.addEventListener('click', () => {
             windows.forEach(item => {
                 item.style.display = 'none';
@@ -40,10 +30,8 @@ const modals = () => {
 
             modal.style.display = "none";
             document.body.style.overflow = "";
-            // document.body.classList.remove('modal-open');
         });
 
-        // закриття модального вікна при натисканні на подложку
         modal.addEventListener('click', (e) => {
             if(e.target === modal && closeClickOverlay) {
                 windows.forEach(item => {
@@ -51,12 +39,10 @@ const modals = () => {
                 });
                 modal.style.display = "none";
                 document.body.style.overflow = "";
-                // document.body.classList.remove('modal-open'); 
             }
         });
     }
     
-    // відкриття модального вікна через певний проміжок часу
     function showModalByTime(selector, time) {
         setTimeout(function() {
             document.querySelector(selector).style.display = "block";
